0c70b27915f01f83ab55475a71b0b473ccbf3794
rightokon3 <rightokon3@gmail.com>
2025-01-29 11:37:57 +0100

    Initial commit

---
+++ src/Me.tsx
@@ -0,0 +1,16 @@
+import {useState} from "react";
+import './index.css'
+import App from "./App";
+
+function Me(){
+    return(
+        <div className="search">
+        <label>
+           <input type="search" name="search" id="" placeholder="search" />
+           
+        </label>
+    </div>
+
+    )
+}
+export default Me
